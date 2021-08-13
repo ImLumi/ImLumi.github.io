dc398d377243abfee3a199a416ad7b5f688cfc16
Bence <mikesimon150@gmail.com>
2021-08-13 22:35:46 +0200

    modositas

---
+++ main.js
@@ -0,0 +1,18 @@
+function calcAmount() {
+    let price = 1000;
+    let amountInput = document.querySelector("input[name='amount-input']");
+    let showAmount = document.querySelector("span.show-amount");
+    let amountNumber = isNaN(parseInt(amountInput.value)) ? 0 : parseInt(amountInput.value)  ;
+    let amount = amountNumber * price;
+
+    if (amountNumber > 10) {
+        alert("Maximum 10 terméket vásárolhat!");
+        return;
+    } else if (amountNumber < 1) {
+        alert("Minimum 1 terméket kell vásárolnia!")
+        return;
+    }
+
+    showAmount.innerHTML = amount;
+}
+
